f1f61bd231aad68743000aa92f7912e0e77042ee
prjwl4 <prjwl4@gmail.com>
2021-07-17 13:34:48 +0530

    Add project files.

---
+++ GZipHelper.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
+
+namespace GZipper
+{
+    public static class GZipHelper
+    {
+        public static string Compress(string sourceFilePath, string targetFilePath)
+        {
+            string compressedFilePath = String.Empty;
+
+            FileInfo sourceFile = new FileInfo(sourceFilePath);
+
+            string targetZipLocation = $"{targetFilePath}{sourceFile.Name}.gz";
+
+            FileInfo compressedFile = new FileInfo(targetZipLocation);
+
+            using(FileStream sourceStream = sourceFile.OpenRead())
+            {
+                using (FileStream targetStream = compressedFile.Create())
+                {
+                    using (GZipStream zipStream = new GZipStream(targetStream, CompressionMode.Compress))
+                    {
+                        try
+                        {
+                            sourceStream.CopyTo(zipStream);
+                            compressedFilePath = compressedFile.FullName;
+                        }
+                        catch (Exception ex)
+                        {
+                            throw ex;
+                        }
+                        
+                    }
+                }
+            }
+
+            return compressedFilePath;
+        }
+
+        public static string Decompress(string sourceFilePath, string targetFilePath)
+        {
+            string decompressedFilePath = String.Empty;
+
+            FileInfo compressedFile = new FileInfo(sourceFilePath);
+
+            using (FileStream sourceStream = compressedFile.OpenRead())
+            {
+                string decompressedFileName = targetFilePath + compressedFile.Name.Replace(".gz","");
+
+                using (FileStream targetStream = File.Create(decompressedFileName))
+                {
+                    using (GZipStream zipStream = new GZipStream(sourceStream, CompressionMode.Decompress))
+                    {
+                        try
+                        {
+                            zipStream.CopyTo(targetStream);
+                            decompressedFilePath = decompressedFileName;
+                        }
+                        catch (Exception ex)
+                        {
+                            throw ex;
+                        }
+                        
+                    }
+                }
+
+            }
+
+            return decompressedFilePath;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GZipper
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Set file paths
+            string sourceFilePath = $"D:\\Programming\\.NET Standard\\GZipper\\SourceFiles\\Sample.txt";
+            string compressedFileDirectory = $"D:\\Programming\\.NET Standard\\GZipper\\CompressedFiles\\";
+            string decompressedFileDirectory = $"D:\\Programming\\.NET Standard\\GZipper\\DecompressedFiles\\";
+
+            // 2. Compress File
+            string compressedFilePath = GZipHelper.Compress(sourceFilePath, compressedFileDirectory);
+
+            // 3. Decompress File
+            string decompressedFileLocation = GZipHelper.Decompress(compressedFilePath, decompressedFileDirectory);
+        }
+    }
+}
